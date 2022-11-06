edae1e9fb0a283feb3b3e03882ecaf74cd6d76cc
Mateus Miqueias <devmiqueias@gmail.com>
2022-11-06 20:21:40 -0300

    build: define window matchmedia property

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,17 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }))
+})
