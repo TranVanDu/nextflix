cc871ed35da2f12727b7a34549a8364744350f57
Mateus Miqueias <devmiqueias@gmail.com>
2023-01-09 19:23:22 -0300

    chore: delete home page

--- src/pages/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import Home from './index.page'
-
-describe('<Home />', () => {
-  it('should render without errors', () => {
-    const { getByText } = render(<Home />)
-    expect(getByText('Home Page')).toBeInTheDocument()
-  })
-})
